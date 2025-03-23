96a9f73f9fa99caae1611e81d4c0b1664b37dea6
PESCE-FOSS <foss@pesce.ac.in>
2025-03-23 11:47:05 +0530

    Update settings.py

--- FOSS/settings.py
+++ FOSS/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
